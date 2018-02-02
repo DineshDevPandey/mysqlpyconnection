214385aac70e120cbc3434330e8483a657716fcf
Dinesh Dev Pandey <pandey@psinc.jp>
2018-02-02 11:30:36 +0900

    show tables, close connection

--- mysql_lib/mysql_operations.py
+++ mysql_lib/mysql_operations.py
@@ -12,15 +12,26 @@ class DbOperations:
     def db_connect(self, host, db_name):
         try:
             DbOperations.db_conn = MySQLdb.connect(host, DbOperations.user, DbOperations.password, db_name)
-            print("Connected to DB :\n\n")
+            print("\nConnected to DB :\n")
         except MySQLdb.Error:
             print("Db connection error :" % MySQLdb.Error)
 
     def show_db(self):
         cur = DbOperations.db_conn.cursor()
         cur.execute("show databases")
-        print("Databases : \n\n")
+        print("\nDatabases : \n")
         for row in cur.fetchall():
             print(row[0])
 
+
+
+    def show_tables(self):
+        cur = DbOperations.db_conn.cursor()
+        cur.execute("show tables")
+        print("\nTables :\n")
+        for row in cur.fetchall():
+            print(row[0])
+
+    def close_connection(self):
         DbOperations.db_conn.close()
+        print("\nConnection cloased :\n")
