2e4633b8887ad564f71470366d536e297d8589f9
Dinesh Dev Pandey <pandey@psinc.jp>
2018-02-02 10:44:08 +0900

    main function calling different MySQL operations

---
+++ mysql_connection/main.py
@@ -0,0 +1,25 @@
+from mysql_lib.mysql_operations import DbOperations
+
+
+class MysqlDb:
+    """
+    Contains database related information
+    """
+    host = ''
+    db_name = ''
+    table_name = ''
+
+    def __init__(self, host, db_name):
+        self.host = host
+        self.db_name = db_name
+
+    def connect(self):
+        db_conn = DbOperations()
+        db_conn.db_connect(self.host, self.db_name)
+        db_conn.show_db()
+
+
+if __name__ == "__main__":
+    db = MysqlDb("localhost", "rc_admin_test")
+    db.connect()
+
